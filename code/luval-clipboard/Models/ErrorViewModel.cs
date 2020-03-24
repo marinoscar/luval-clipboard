ef312e96d75e19307f0ac549cb7df15fb3702dae
marinoscar <oscar@marin.cr>
2020-03-24 15:17:39 -0500

    initial commit

---
+++ code/luval-clipboard/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace luval_clipboard.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
